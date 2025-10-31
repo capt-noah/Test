fa0fb6e6a1b07e346e0a41a99209833f06c7db15
Alem-Mamo <119974698+Alem-Mamo@users.noreply.github.com>
2025-10-31 15:55:05 +0300

    merged the host and port for testing

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,10 @@ const mysql = require('mysql2')
 require('dotenv').config()
 
 const pool = mysql.createConnection({
-    host: process.env.HOST || 'mysql-db02.remote',
+    host: process.env.HOST || 'mysql-db02.remote:32636',
     user: process.env.DB_USER || 'Noah',
     database: process.env.DATABASE || 'lidetain_',
     password: process.env.PASSWORD || 'rS4ZoAcp!jt%ic43',
-    port: process.env.DB_PORT || 32636,
 })
 
 const db = pool.promise()
