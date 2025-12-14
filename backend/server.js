6e7757338d67605a7e5ad0db77cd83cea928e0cf
Alem-Mamo <119974698+Alem-Mamo@users.noreply.github.com>
2025-12-14 19:20:06 +0300

    the server works so back at making db work

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const path = require('path')
+const db = require('./db.js')
 require('dotenv').config()
 
 app.use(express.json())
