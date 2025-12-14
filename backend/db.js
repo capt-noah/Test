179dee0117b185d51acdfbba22f7d80971d6e9e5
Alem-Mamo <119974698+Alem-Mamo@users.noreply.github.com>
2025-12-14 20:45:15 +0300

    ,

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2')
 require('dotenv').config()
 
 const pool = mysql.createConnection({
-    host: 'localhost'
+    host: 'localhost',
     user: 'lideta',
     database: 'lideta_db',
     password: 'vuV2hM3hGan7!k#v',
