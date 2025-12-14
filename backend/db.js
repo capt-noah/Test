f34882031cd0c30d71a27fa98de1520f36898456
Alem-Mamo <119974698+Alem-Mamo@users.noreply.github.com>
2025-12-14 20:44:59 +0300

    c

--- backend/db.js
+++ backend/db.js
@@ -2,8 +2,7 @@ const mysql = require('mysql2')
 require('dotenv').config()
 
 const pool = mysql.createConnection({
-    host: 'mysql-db02.remote',
-    port: '32636',
+    host: 'localhost'
     user: 'lideta',
     database: 'lideta_db',
     password: 'vuV2hM3hGan7!k#v',
