e5ea702932b4b3e02b347b072b6dc54fea52b45d
Alem-Mamo <119974698+Alem-Mamo@users.noreply.github.com>
2025-12-14 18:52:33 +0300

    updated the connection credentials

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2')
 require('dotenv').config()
 
 const pool = mysql.createConnection({
-    host: process.env.HOST || 'localhost',
-    user: process.env.DB_USER || 'Noah',
-    database: process.env.DATABASE || 'lidetain_',
-    password: process.env.PASSWORD || 'rS4ZoAcp!jt%ic43',
+    host: 'localhost',
+    user: 'lideta',
+    database: 'lideta_db',
+    password: '$b3V296wi',
 })
 
 const db = pool.promise()
