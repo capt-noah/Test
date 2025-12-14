8db5e4cb5fbbe7ab8fd8bbd82a81d93532567d10
Alem-Mamo <119974698+Alem-Mamo@users.noreply.github.com>
2025-12-14 19:21:02 +0300

    trying 127.0.0.1 port

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2')
 require('dotenv').config()
 
 const pool = mysql.createConnection({
-    host: 'mysql-db02.remote',
+    host: '127.0.0.1',
     port: 32636,
     user: 'lideta',
     database: 'lideta_db',
