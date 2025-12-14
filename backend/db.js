fdc9da949fdb475cd8733d2cf2d05c67bd738e6d
Alem-Mamo <119974698+Alem-Mamo@users.noreply.github.com>
2025-12-14 19:59:21 +0300

    trying using the ip

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2')
 require('dotenv').config()
 
 const pool = mysql.createConnection({
-    host: 'mysql-db02.remote',
+    host: '213.55.96.152',
     port: 32636,
     user: 'lideta',
     database: 'lideta_db',
