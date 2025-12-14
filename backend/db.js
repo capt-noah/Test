5848e0edd4bb6b81032773309f6b4f8c756a1ef6
Alem-Mamo <119974698+Alem-Mamo@users.noreply.github.com>
2025-12-14 19:22:42 +0300

    trying localhost

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2')
 require('dotenv').config()
 
 const pool = mysql.createConnection({
-    host: '127.0.0.1',
+    host: 'localhost',
     port: 32636,
     user: 'lideta',
     database: 'lideta_db',
