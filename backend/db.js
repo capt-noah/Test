1d9b65566ab929c20b27c92818cc6b7ebac4c2ee
Alem-Mamo <119974698+Alem-Mamo@users.noreply.github.com>
2025-12-14 19:28:50 +0300

    removed port

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 
 const pool = mysql.createConnection({
     host: 'localhost',
-    port: 32636,
     user: 'lideta',
     database: 'lideta_db',
     password: '$b3V296wi',
