52477b2036f0f12ed62f7e0ee51891166d8e2764
Alem-Mamo <119974698+Alem-Mamo@users.noreply.github.com>
2025-12-14 19:06:12 +0300

    chnaged the host

--- backend/db.js
+++ backend/db.js
@@ -2,8 +2,7 @@ const mysql = require('mysql2')
 require('dotenv').config()
 
 const pool = mysql.createConnection({
-    host: 'localhost',
-    port: '32636',
+    host: 'mysql-db02.remote:32636',
     user: 'lideta',
     database: 'lideta_db',
     password: '$b3V296wi',
